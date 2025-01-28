c11666e97a737089c0922ecb7c8cde5347f69544
Igor D <kek>
2025-01-28 19:47:25 +0300

    lol

---
+++ .terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version     = "0.135.0"
+  constraints = "~> 0.88"
+  hashes = [
+    "h1:fiLfErj+TUHPV2eUmmX9lWtsopOGxO8m7ZvQAqxLKHg=",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.88"
+    }
+  }
+}
+provider "yandex" {
+  token     = "y0__xCA449iGMHdEyCzqcmQEs4kHSEniIHGPEh9fmU9cSN6S_2z"
+  cloud_id  = "b1g2plbt4oav513ephpl"
+  folder_id = "b1gjm8ap3mu8tgq2irn5"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_compute_disk" "boot-disk-1" {
+  name     = "boot-disk-1"
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  size     = "13"
+  image_id = "fd81n0sfjm6d5nq6l05g"
+}
+
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores         = 2
+    memory        = 3
+    core_fraction = 20
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-1.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  allow_stopping_for_update = true
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
